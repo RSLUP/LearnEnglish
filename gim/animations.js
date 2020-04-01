b73b7a73cdc6a30656e3a059784e272ca138a1f2
Gim kelum <49275770+gim96@users.noreply.github.com>
2020-04-01 12:55:51 +0530

    added aminations

---
+++ gim/animations.js
@@ -0,0 +1,14 @@
+
+function openLink(evt, animName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(animName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
