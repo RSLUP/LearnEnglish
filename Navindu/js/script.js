5e9dda789a0b4541fd4174b9582492592cfff45b
NavinduK <navindu.nk@gmail.com>
2020-04-04 21:16:24 +0530

    Activity 7.0 Final commit

---
+++ Navindu/js/script.js
@@ -0,0 +1,75 @@
+var modal1 = document.getElementById("Modal1");
+var modal2 = document.getElementById("Modal2");
+var modal3 = document.getElementById("Modal3");
+var modal4 = document.getElementById("Modal4");
+var modal5 = document.getElementById("Modal5");
+var modal6 = document.getElementById("Modal6");
+var modal7 = document.getElementById("Modal7");
+var modal8 = document.getElementById("Modal8");
+var modal9 = document.getElementById("Modal9");
+var modal10 = document.getElementById("Modal10");
+var modal11 = document.getElementById("Modal11");
+var modal12 = document.getElementById("Modal12");
+
+function clickfunc1(){
+    modal1.style.display = "block";
+}
+
+function clickfunc2(){
+    modal2.style.display = "block";
+}
+
+function clickfunc3(){
+    modal3.style.display = "block";
+}
+
+function clickfunc4(){
+    modal4.style.display = "block";
+}
+
+function clickfunc5(){
+    modal5.style.display = "block";
+}
+
+function clickfunc6(){
+    modal6.style.display = "block";
+}
+
+function clickfunc7(){
+    modal7.style.display = "block";
+}
+
+function clickfunc8(){
+    modal8.style.display = "block";
+}
+
+function clickfunc9(){
+    modal9.style.display = "block";
+}
+
+function clickfunc10(){
+    modal10.style.display = "block";
+}
+
+function clickfunc11(){
+    modal11.style.display = "block";
+}
+
+function clickfunc12(){
+    modal12.style.display = "block";
+}
+
+function closefunc() {
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+	modal3.style.display = "none";
+	modal4.style.display = "none";
+	modal5.style.display = "none";
+	modal6.style.display = "none";
+	modal7.style.display = "none";
+	modal8.style.display = "none";
+	modal9.style.display = "none";
+	modal10.style.display = "none";
+	modal11.style.display = "none";
+	modal12.style.display = "none";
+}
