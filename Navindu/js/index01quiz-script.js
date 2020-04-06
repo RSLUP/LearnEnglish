851ffd0358502e36506541648b3cce8d1518b556
NavinduK <navindu.nk@gmail.com>
2020-04-06 14:09:44 +0530

    28th commit

---
+++ Navindu/js/index01quiz-script.js
@@ -0,0 +1,10 @@
+var modalspr = document.getElementById("modalspr");
+
+function sprQuiz(){
+    modalspr.style.display = "block";
+}
+
+function quizClose() {
+    modalspr.style.display = "none";
+}
+
