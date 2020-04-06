5dad8170a004592281878922d0307ea0e191bd83
BuddhiPrasad <bp980930@gmail.com>
2020-04-06 11:15:27 +0530

    eighth Commit

---
+++ BuddhiPrasad/js/questions_8speech.js
@@ -0,0 +1,45 @@
+var allQuestions = [{
+    question: "Which part of speech names a person, place, or thing?",
+    option1: "noun",
+    option2: "adjective",
+    option3: "preposition",
+    option4: "verb",
+    answer: 1
+  }, {
+    question: "Which part of speech replaces a noun?",
+    option1: "conjunction",
+    option2: "adverb",
+    option3: "pronoun",
+    option4: "preposition",
+    answer: 3
+  }, {
+    question: " Which part of speech describes a noun or a pronoun?",
+    option1: "adjective",
+    option2: "adverb",
+    option3: "conjunction",
+    option4: "verb",
+    answer: 1
+  },{
+    question: "Which part of speech modifies a verb, adjective, or adverb?",
+    option1: "preposition",
+    option2: " verb",
+    option3: "noun",
+    option4: "adverb",
+    answer: 4
+  },{
+    question: "Define:  Verb",
+    option1: "An action",
+    option2: "A word that gives more detail about nouns",
+    option3: "A person, place, thing, or idea",
+    option4: "A word that gives more details about verbs",
+    answer: 1
+  },{
+    question: "Used as a lubricant",
+    option1: "Graphite",
+    option2: "Silica",
+    option3: "Iron Oxide",
+    option4: "Diamond",
+    answer: 0
+  
+
+}];
