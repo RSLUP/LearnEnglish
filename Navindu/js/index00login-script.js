745d7ea54c4ee451dbef574881990c7d6b0fe7b3
NavinduK <navindu.nk@gmail.com>
2020-04-10 01:44:02 +0530

    35th commit

--- Navindu/js/index00login-script.js
+++ Navindu/js/index00login-script.js
@@ -1,4 +1,22 @@
-function redirect() {
-	alert();
-	location.replace("index01.html");
+var form;
+
+function redirect(form){
+
+	var user="RSLUP";
+	var pass="r5Lup@Ac9"
+
+	if(form.email.value==user && form.pass.value==pass){
+		var logintime = new Date();
+
+		localStorage.setItem("logUser", user);
+		localStorage.setItem("logtime", logintime.toDateString());
+		//alert(logtime.toDateString());
+
+		window.location.href = "index01.html";
+	}
+	
+	else{
+		document.getElementById("Error").innerHTML="Incorrect username or password.";
+	}
+
 }
