05f636b80f2d7d04a1e77f7422ef219469c4fb3e
viranga <virangapasindu4@gmail.com>
2020-04-09 15:25:57 +0530

    login done 1

--- Viranga/Login/script.js
+++ Viranga/Login/script.js
@@ -18,3 +18,41 @@ inputs.forEach(input => {
     input.addEventListener('focus', focusFunc);
     input.addEventListener('blur', blurFunc);
 });
+
+//login code
+
+const txtuser = document.querySelector(".txtuser");
+const txtpass = document.querySelector(".txtpass");
+const submit = document.querySelector(".submit");
+const message = document.querySelector(".message");
+const form = document.querySelector(".form");
+
+
+const user = "RSLUP";
+const pass = "r5Lup@Ac9";
+
+submit.addEventListener("click",(e)=>
+{
+
+    if(user===txtuser.value && pass===txtpass.value)
+    {
+        
+        
+        window.location.href = "speech.html";
+        return;
+    }
+    if(user===txtuser.value && pass!==txtpass.value)
+    {
+        message.innerHTML = "Invalid password!";
+        return;
+    }
+    else
+    
+    {
+        message.innerHTML = "Invalid username and password!";
+    
+    }
+});
+
+
+
