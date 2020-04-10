b3ab350b3fede6624e76624121d4cc5a2c0c8fde
Ravindu97 <f.ravindu@users.noreply.github.com>
2020-04-10 11:48:16 +0530

    still correcting some bugs

--- Ravindu/script/loginScript.js
+++ Ravindu/script/loginScript.js
@@ -8,19 +8,25 @@ const pass = "r5Lup@Ac9";
 
 function login(){
 
-  const usernminp = document.querySelector(".usernminp");
-  const passinp = document.querySelector(".passinp");
+
+  //QUERYSELECTORS are not working
+  // var usernminp = document.querySelector(".usernminp");
+  // var passinp = document.querySelector(".passinp");
+
+   var username=document.login_form.usernminp.value;
+   var password = document.login_form.passinp.value;
+
 
 
   if (user == usernminp && pass == passinp) {
 
-
+window.open("index.html");
 
     //if data are correct store them in local
     localStorage.setItem("userName", usernminp);
     localStorage.setItem("logDate", date);
 
-    window.open("index.html");
+
   }else
   {
       message.innerHTML = "Invalid username and password!";
