db5691f91f716e87caa4389ef4382e3f8b27f1eb
viranga <virangapasindu4@gmail.com>
2020-04-15 18:11:54 +0530

    add timer

--- Viranga/games/js/wordselector.js
+++ Viranga/games/js/wordselector.js
@@ -2,6 +2,38 @@ const cards = document.querySelectorAll(".memory-card");
 let hasFlippedCard = false;
 let lockBoard=false;
 let firstCard, secondCard;
+const timeOut = document.querySelector(".timeOut");
+const start = document.querySelector(".start");
+
+//set timer
+var timer = setInterval(function(){gameTimer()}, 1000);
+var seconds = 59;
+
+function gameTimer()
+{
+    if(seconds ===0)
+    {
+        gameStop();
+        timeOut.classList.add("show");
+    }
+    document.querySelector(".time").innerHTML = "00:" + zeroPad(seconds,2)
+    seconds= seconds -1;
+}
+
+function gameStop()
+{
+    clearInterval(timer);
+}
+function zeroPad(num,places)
+{
+    var zero = places-num.toString().length +1;
+    return Array(+(zero > 0 && zero)).join("0")+ num;
+}
+
+
+
+
+//flip card
 function flipCard()
 {
     if(lockBoard) return;
@@ -24,6 +56,7 @@ function flipCard()
   
 }
 
+//check for cards match
 function checkForMatch()
 {
     if(firstCard.dataset.card===secondCard.dataset.card)
@@ -37,6 +70,8 @@ function checkForMatch()
     }
 }
 
+
+//disable once they were clicked
 function disableCards()
 {
     firstCard.removeEventListener('click', flipCard);
@@ -54,19 +89,37 @@ function unFlipCards()
     },1200);
 }
 
+//reset 
 function resetBoard()
 {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard ,secondCard] = [null, null];
 }
 
-(function shuffle()
+
+//start button
+function getStart()
 {
+    start.classList.remove("hide");
+    shuffle();
+    
+}
+//shuffle the cards
+function shuffle()
+{
+    gameTimer();
+    
     cards.forEach(card => 
     {
         let randomPos = Math.floor(Math.random()*12);
         card.style.order = randomPos;
     });
-})();
+}
+
+//button try again
+function tryAgain()
+{
+    window.location.reload();
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
