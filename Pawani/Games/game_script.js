848f4ccc25617518b3cd693745dcc0a80fbeeb45
PawaniPubudika <pubudikalwp@gmail.com>
2020-04-15 22:36:24 +0530

    Activity 9.1-third commit

---
+++ Pawani/Games/game_script.js
@@ -0,0 +1,19 @@
+function TicTacToegame() {
+    const board=new Board();
+    const humanplayer=new HumanPlayer();
+    const computerplayer=new ComputerPlayer();
+
+}
+
+
+function Board() {
+
+}
+
+function HumanPlayer() {
+
+}
+
+function ComputerPlayer() {
+
+}
