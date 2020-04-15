b99857cb0d6b9eb96ce06fa14059705dab9c1a92
ama926 <amaprabhashini926@gmail.com>
2020-04-15 11:27:38 +0530

    minigame-basic v5.0.1

---
+++ Ama/miniGame/minigames.js
@@ -0,0 +1,12 @@
+function getwinner()
+{
+    var box1 = document.getElementById("box1"),
+        box2 = document.getElementById("box2"),
+        box3 = document.getElementById("box3"),
+        box4 = document.getElementById("box4"),
+        box5 = document.getElementById("box5"),
+        box6 = document.getElementById("box6"),
+        box7 = document.getElementById("box7"),
+        box8 = document.getElementById("box8"),
+        box9 = document.getElementById("box9"),
+}
