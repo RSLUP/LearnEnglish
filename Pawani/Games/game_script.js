220dc8b49dff32fe1538adad2785b1c792fdc4e9
PawaniPubudika <pubudikalwp@gmail.com>
2020-04-17 17:52:44 +0530

    Activity 9.1-fifth commit

--- Pawani/Games/game_script.js
+++ Pawani/Games/game_script.js
@@ -26,24 +26,3 @@
     };
 
     enableListeners();
-
-    // const qNumID=function () {
-    //
-    // }
-    //
-    // const emptyQs = function() {
-    //     return grid().filter(_qEl => _qEl.innerText === '');
-    // }
-    //
-    // const allSame = function(arr){
-    //     return arr.every(_qEl => _qEl.innerText === arr[0].innerText && _qEl.innerText !== '');
-    // }
-    //
-    // const takeTurn =function(index, letter) {
-    //     return grid()[index].innerText = letter;
-    // }
-    // const opponentChoice = function() {
-    //     return qNumId(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
-    // }
-
-    
