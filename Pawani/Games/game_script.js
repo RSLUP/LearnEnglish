7f7d485bb89026a03e6d6b35cc3e18af9722f0b8
PawaniPubudika <pubudikalwp@gmail.com>
2020-04-17 13:08:56 +0530

    Activity 9.1-fourth commit

--- Pawani/Games/game_script.js
+++ Pawani/Games/game_script.js
@@ -1,19 +1,49 @@
-function TicTacToegame() {
-    const board=new Board();
-    const humanplayer=new HumanPlayer();
-    const computerplayer=new ComputerPlayer();
+    const winningCombs =[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ];
 
-}
+    const grid=function () {
+        return Array.from(document.getElementsByClassName("q"));
+    };
+
+    const clickFn = function($event){
+        //return console.log($event.target);
+    };
 
 
-function Board() {
+    const enableListeners=function () {
+        return grid().forEach(_qEl => _qEl.addEventListener('click', clickFn));
+    };
+    const disableListeners = function() {
+        return grid().forEach(_qEl => _qEl.removeEventListener('click', clickFn));
+    };
 
-}
+    enableListeners();
 
-function HumanPlayer() {
+    // const qNumID=function () {
+    //
+    // }
+    //
+    // const emptyQs = function() {
+    //     return grid().filter(_qEl => _qEl.innerText === '');
+    // }
+    //
+    // const allSame = function(arr){
+    //     return arr.every(_qEl => _qEl.innerText === arr[0].innerText && _qEl.innerText !== '');
+    // }
+    //
+    // const takeTurn =function(index, letter) {
+    //     return grid()[index].innerText = letter;
+    // }
+    // const opponentChoice = function() {
+    //     return qNumId(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
+    // }
 
-}
-
-function ComputerPlayer() {
-
-}
+    
