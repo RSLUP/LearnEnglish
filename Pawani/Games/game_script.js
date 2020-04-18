7c69757a71dd8fae501ee9d83d7e80c3bb11fc4b
PawaniPubudika <pubudikalwp@gmail.com>
2020-04-18 20:12:32 +0530

    Activity 9.1-started again-first commit-added eventListeners,click function

--- Pawani/Games/game_script.js
+++ Pawani/Games/game_script.js
@@ -1,57 +1,27 @@
 
-
-    const clickFn = function($event){
-    //return console.log($event.target);
-    takeTurn(qNumId($event.target),'X');
-    opponentTurn();
-    };
+const grid=function () {
+    return Array.from(document.getElementsByClassName("q"));
+};
 
 
-    const grid=function () {
-        return Array.from(document.getElementsByClassName("q"));
-    };
+const clickFn = function($event){
+    return console.log($event.target);
+};
 
-    const qNumId = function (qEl) {
-        return Number.parseInt(qEl.id.replace('q', ''));
-    };
+const enableListeners=function () {
+    return grid().forEach(adding);
+};
 
-    const emptyQs=function() {
-        return grid().filter(checkEmpty);
-    };
-    function checkEmpty(qelement){
-        if(qelement.innerText==='') {
-            return qelement;
-        }
-    }
+function adding(qelement) {
+    return qelement.addEventListener('click', clickFn);
+}
 
+const disableListeners = function() {
+    return grid().forEach(removing);
+};
 
-    const takeTurn = function(index, letter){
-        return grid()[index].innerText = letter
-    };
+function removing(qelement) {
+    return qelement.removeEventListener('click', clickFn);
+}
 
-    const opponentTurn = function(){
-        disableListeners();
-        setTimeout(function () {
-            takeTurn(opponentChoice(), 'O');
-            enableListeners();
-        }, 1000);
-    };
-
-    const opponentChoice =function () {
-        return qNumId(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
-    };
-
-
-
-    // adding eventlisteners........
-
-    const enableListeners=function () {
-        return grid().forEach(qelement => qelement.addEventListener('click', clickFn));
-    };
-    const disableListeners = function() {
-        return grid().forEach(qelement => qelement.removeEventListener('click', clickFn));
-    };
-
-    enableListeners();
-
-
+enableListeners();
