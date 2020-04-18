c7ec0bbff31f552ad0e905b6bd18b3e35eff0bf2
ama926 <amaprabhashini926@gmail.com>
2020-04-18 18:44:15 +0530

    jq suggestions added v5.0.2

--- Ama/miniGame/minigames.js
+++ Ama/miniGame/minigames.js
@@ -1,12 +1,15 @@
-function getwinner()
-{
-    var box1 = document.getElementById("box1"),
-        box2 = document.getElementById("box2"),
-        box3 = document.getElementById("box3"),
-        box4 = document.getElementById("box4"),
-        box5 = document.getElementById("box5"),
-        box6 = document.getElementById("box6"),
-        box7 = document.getElementById("box7"),
-        box8 = document.getElementById("box8"),
-        box9 = document.getElementById("box9"),
-}
+
+/*var count = 0;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count++;
+  if (count === 10){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    // or...
+    alert("You're out of time!");
+  }
+}, 1000);*/
+
+
+
