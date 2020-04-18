586c20fa8645c548ab6c7023143106c785214bf0
PawaniPubudika <pubudikalwp@gmail.com>
2020-04-18 21:04:00 +0530

    Activity 9.1-third commit-added string converting function

--- Pawani/Games/game_script.js
+++ Pawani/Games/game_script.js
@@ -2,6 +2,13 @@ const grid=function () {
     return Array.from(document.getElementsByClassName("q"));
 };
 
+// replace id 'q' with ' ' & convert that string to integer
+const qNumId = function (qEl) {
+    return Number.parseInt(qEl.id.replace('q', ''));
+};
+
+
+
 const clickFn = function($event){
     return console.log($event.target);
 };
