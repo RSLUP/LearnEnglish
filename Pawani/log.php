532083ec1e6c210bc4000041dd37b2a24658b21b
PawaniPubudika <pubudikalwp@gmail.com>
2020-05-11 02:01:02 +0530

    activity 14-second commit

--- Pawani/log.php
+++ Pawani/log.php
@@ -8,3 +8,11 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
+//create check_input function
+function check_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
