6f50399375335a64f5e1095e99633dde693bc95f
Anjali Jayasinghe <anjalidilrangi@gmail.com>
2020-05-13 17:40:02 +0530

    correcting session

--- Anjali/javascriptLogin.js
+++ Anjali/javascriptLogin.js
@@ -1,14 +1,6 @@
-/*function validateData()
+function validateData()
 	{
-        if(!(document.login.txtUsername.value == "RSLUP" && document.login.txtPassword.value == "r5Lup@Ac9"))
-        {
-            alert("Invalid Username and Password!");
-            document.login.txtUsername.focus() ;
-            return false;
-        }
-        else{
-            var name = document.login.txtUsername.value;
-            localStorage.setItem("name", name);*/
+      
             var date = new Date().toISOString().split('T');
             localStorage.setItem("date",date); 
             window.location.href ="englishIndex.php";  
